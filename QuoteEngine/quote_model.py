be5f61ae555cfcb0b74ffc8103c7c9a7bce82f2b
Maurits <mcb.westbroek@gmail.com>
2022-12-25 11:56:53 +0100

    Add class docstring

--- QuoteEngine/quote_model.py
+++ QuoteEngine/quote_model.py
@@ -1,4 +1,12 @@
 class QuoteModel:
+    """Class for containing quotes."""
+
     def __init__(self, body: str, author: str) -> None:
+        """
+        Init method for class for containing quotes.
+        
+        :param body:    The quote content, like "Some quote" in "Some quote" - Author 
+        :param author:  The quote's "author", like Author in "Some quote" - Author
+        """
         self.body = body
         self.author = author
