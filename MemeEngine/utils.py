b381f6b9352caa1dc3d8f01aa37318955ba45f52
Maurits <mcb.westbroek@gmail.com>
2022-12-25 11:56:45 +0100

    Refactor random string into utils.py

---
+++ MemeEngine/utils.py
@@ -0,0 +1,11 @@
+from random import choice
+from string import ascii_uppercase, digits
+
+def generate_random_string(length: int = 5) -> str:
+    """
+    Generate a random string of uppercase numbers and digits.
+
+    :param length:  the length of the desired string
+    :returns str:   a random string
+    """
+    return ''.join(choice(ascii_uppercase + digits) for _ in range(length))
