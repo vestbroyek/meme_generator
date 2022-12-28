42589747f41930043efe7c2b6acd1575d6833f6c
Maurits <mcb.westbroek@gmail.com>
2022-12-28 10:40:42 +0000

    Remove references to dotenv

--- MemeEngine/__init__.py
+++ MemeEngine/__init__.py
@@ -1,3 +1,1 @@
 from .meme_engine import MemeEngine
-from dotenv import load_dotenv
-load_dotenv()
--- QuoteEngine/__init__.py
+++ QuoteEngine/__init__.py
@@ -1,4 +1,2 @@
 from .ingestors import Ingestor
 from .quote_model import QuoteModel
-from dotenv import load_dotenv
-load_dotenv()
